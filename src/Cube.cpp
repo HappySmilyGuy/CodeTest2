08f1871bf8a461fdd9b70a3279763d0906461218
Edward Latimer <edwardlatimer@hotmail.co.uk>
2016-10-17 18:30:22 +0100

    Written is_solved, writing rotate_face.

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -19,9 +19,8 @@ namespace RubiksCube
   {}
 
   Cube::Cube(const Cube& in)
-  {
-    // TODO
-  }
+    : faces_(in.view())
+  {}
 
   Cube::~Cube()
   {}
@@ -33,18 +32,76 @@ namespace RubiksCube
 
   void Cube::rotate_face(Face face, Direction direction)
   {
-    //TODO
+    Cube_Of_Faces temp;
+    for (int x = 0; x < 3; ++x)
+    {
+      for (int y = 0; y < 3; ++y)
+      {
+        if (direction == CLOCKWISE)
+        {
+          temp[face][x][y] = faces_[face][x][2 - y];
+        }
+        else if (direction == COUNTER_CLOCKWISE)
+        {
+          temp[face][x][y] = faces_[face][2 - x][y];
+        }
+        else return; // should never happen: incorrect input.
+      }
+    }
+    for (int x = 0; x < 3; ++x)
+    {
+      for (int y = 0; y < 3; ++y)
+      {
+        faces_[face][x][y] = temp[face][x][y];
+      }
+    }
+    // TODO rotate connected edge squares
+    switch (face)
+    {
+      case FRONT:
+
+        break;
+      case RIGHT:
+
+        break;
+      case BACK:
+
+        break;
+      case LEFT:
+
+        break;
+      case UP:
+
+        break;
+      case DOWN:
+
+        break;
+
+    }
   }
 
-  void Cube::rotate_cube(Axis, Direction)
+  void Cube::rotate_cube(Axis axis, Direction dir)
   {
-    // TODO
+
   }
 
   bool Cube::is_solved() const
   {
-    // TODO
-    return false;
+    for (int f = 0; f < 6; ++f)
+    {
+      int face_value = faces_[f][0][0];
+      for (int x = 0; x < 3; ++x)
+      {
+        for (int y = 0; y < 3; ++y)
+        {
+          if (faces_[f][x][y] != face_value)
+          {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
   }
 
   Cube_Of_Faces Cube::view() const
